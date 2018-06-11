022c09a5082d81d578559668a16d0758b489cc4e
Alistair Shepherd <alistair@accudio.com>
2018-06-11 23:34:19 +0100

    Work on design of list and dropdown options

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import 'simplebar';
-import 'simplebar/dist/simplebar.css';
+import Select from 'react-select';
 import SpotifyWebApi from 'spotify-web-api-js';
+import 'simplebar';
+import 'react-select/dist/react-select.css';
+import 'simplebar/dist/simplebar.css';
+
+import './fonts.css';
 import Util from './util';
 import Background from './Background';
 import './App.css';
@@ -11,20 +15,29 @@ const spotifyApi = new SpotifyWebApi();
 function TrackItem(props) {
   const name = props.obj.name
   const album = props.obj.album.name;
-  const artistTitle = props.obj.artists.length > 1 ? 'Artists': 'Artist';
   const artists = Util.artistsToString(props.obj.artists);
   const genres = Util.genresToString(props.obj.genres);
   const url = props.obj.external_urls.spotify;
   const albumArt = props.obj.album.images[0].url;
   return (
-    <li>{name}
-      <ul>
-        <li>{artistTitle}: {artists}</li>
-        <li>Album: {album}</li>
-        <li>Genres: {genres}</li>
-        <li><a href={url} target="_blank">Link</a></li>
-        <li><img src={albumArt} alt={album + 'Album Art'} className="album-art" /></li>
-      </ul>
+    <li>
+      <div className="list-item">
+        <div className="list-text">
+          <div className="name">
+            <span>{name}</span>
+          </div>
+          <div className="artist-album">
+            {artists} &bull; {album}
+          </div>
+          <div className="genres">
+            {genres}
+          </div>
+          <div className="link">
+            <a href={url} target="_blank">Open in Spotify</a>
+          </div>
+        </div>
+        <img src={albumArt} alt={album + 'Album Art'} className="album-art" />
+      </div>
     </li>
   );
 }
@@ -41,8 +54,8 @@ class App extends Component {
     }
     this.state = {
       loggedIn: token ? true : false,
-      numResults: 10,
-      timeRange: 'medium_term',
+      numResults: {value: 10, label: 'Top 10 tracks'},
+      timeRange: {value: 'medium_term', label: 'in the past 6 Months'},
       topTracks: null,
       background: {
         element: '#background',
@@ -64,6 +77,10 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    this.getTopTracks(this.state.numResults.value, this.state.timeRange.value);
+  }
+
   getHashParams() {
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g, q = window.location.hash.substring(1);
@@ -75,20 +92,33 @@ class App extends Component {
     return hashParams;
   }
 
-  setNumResults(n) {
-    this.setState({
-      numResults: n
-    });
+  setNumResults = (numResults) => {
+    this.setState({numResults});
+    if ((
+        numResults
+        && typeof numResults.value !== 'undefined'
+        && this.state.timeRange
+        && typeof this.state.timeRange.value !== 'undefined'
+      )) {
+      this.getTopTracks(numResults.value, this.state.timeRange.value);
+    }
   }
 
-  setTimeRange(n) {
-    this.setState({
-      timeRange: n
-    });
+  setTimeRange = (timeRange) => {
+    this.setState({timeRange});
+    if ((
+        this.state.numResults
+        && typeof this.state.numResults.value !== 'undefined'
+        && timeRange
+        && typeof timeRange.value !== 'undefined'
+      )) {
+      this.getTopTracks(this.state.numResults.value, timeRange.value);
+    }
   }
 
-  getTopTracks() {
-    spotifyApi.getMyTopTracks({limit: this.state.numResults, time_range: this.state.timeRange})
+  getTopTracks(numResults, timeRange) {
+    console.log('Making request for '+numResults+' '+timeRange+' results.');
+    spotifyApi.getMyTopTracks({limit: numResults, time_range: timeRange})
       .then((response) => {
         let topTracks = response.items;
         let artists = [];
@@ -107,53 +137,88 @@ class App extends Component {
       });
   }
 
+  loadMore = () => {
+    let newResult = 10;
+    switch(this.state.numResults.value) {
+      case 10:
+        newResult = 20;
+        break;
+      case 20:
+        newResult = 30;
+        break;
+      case 30:
+        newResult = 40;
+        break;
+      case 40:
+        newResult = 50;
+        break;
+      default:
+    }
+    let newResultLabel = 'Top '+newResult+' tracks';
+    this.setState({numResults: {value: newResult, label: newResultLabel}})
+    this.getTopTracks(newResult, this.state.timeRange.value);
+  }
+
   render() {
-    const topTracks = this.state.topTracks;
+    const { numResults, timeRange, topTracks } = this.state;
 
     return (
       <div className='app'>
 
         <Background id="background" config={ this.state.background }></Background>
         <div className="navigation">
+          <div className="menu"></div>
+          <div className="user">
+            <a href='http://localhost:8888/login'>Login to Spotify</a>
+          </div>
         </div>
         <div className="page-wrap">
+
+          <div className="config-options">
+              <Select
+                name="num-results"
+                aria-label="Number of Results"
+                value={numResults}
+                onChange={this.setNumResults}
+                options={[
+                  {value: 10, label: 'Top 10 tracks'},
+                  {value: 20, label: 'Top 20 tracks'},
+                  {value: 50, label: 'Top 50 tracks'},
+                ]}
+                autoBlur
+                clearable={false}
+              />
+              <Select
+                name="time-range"
+                aria-label="Time Range"
+                value={timeRange}
+                onChange={this.setTimeRange}
+                options={[
+                  {value: 'short_term', label: 'in the past 4 Weeks'},
+                  {value: 'medium_term', label: 'in the past 6 Months'},
+                  {value: 'long_term', label: 'of all time'},
+                ]}
+                autoBlur
+                clearable={false}
+              />
+          </div>
+
           <div className="page" data-simplebar>
             <div className="page-content">
-              <a href='http://localhost:8888/login'> Login to Spotify </a>
-
               { this.state.loggedIn &&
                 <div>
-
-                  <div>
-                    
-                    <div>
-                      <button onClick={ () => this.setNumResults(10) }>10</button>
-                      <button onClick={ () => this.setNumResults(20) }>20</button>
-                      <button onClick={ () => this.setNumResults(50) }>50</button>
-                      { this.state.numResults }
-                    </div>
-
-                    <div>
-                      <button onClick={ () => this.setTimeRange('short_term') }>4 Weeks</button>
-                      <button onClick={ () => this.setTimeRange('medium_term') }>6 Months</button>
-                      <button onClick={ () => this.setTimeRange('long_term') }>All Time</button>
-                      { this.state.timeRange }
-                    </div>
-
-                    <button onClick={ () => this.getTopTracks() }>
-                      Check Top Tracks
+                  <ol className="results">
+                    { this.state.topTracks &&
+                      topTracks.map((object, i) => {
+                        return <TrackItem obj={ object } key={ object.id } />
+                      })
+                    }
+                  </ol>
+                  { numResults.value < 50 &&
+                    <button className="view-more" onClick={this.loadMore}>
+                      View More
                     </button>
-
-                    <div>
-                      <ol>
-                        { this.state.topTracks &&
-                          topTracks.map((object, i) => {
-                            return <TrackItem obj={ object } key={ object.id } />
-                          })
-                        }
-                      </ol>
-                    </div>
-                  </div>
+                  }
                 </div>
               }
             </div>
